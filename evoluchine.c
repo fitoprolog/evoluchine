ca5ec37b0a41571789193dacbe8b31a1b8307aeb
fito <fito.prolog@gmail.com>
2021-12-27 10:16:28 -0800

    fuzzy version seems to be working but has very slow convergence

--- evoluchine.c
+++ evoluchine.c
@@ -7,7 +7,8 @@
 #define SUM  2
 #define NEGATE_SUM 3
 
-void evoluchine_randomize(char *operations,int operations_size, int *inputs_order, int input_size){
+
+void evoluchine_randomize(unsigned char *operations,int operations_size, int *inputs_order, int input_size){
   for(int o=0;o!= operations_size;o++){
     if ( random()%2)
         operations[o]=random()%4;
@@ -17,47 +18,47 @@ void evoluchine_randomize(char *operations,int operations_size, int *inputs_orde
   }
 }
 
-char evoluchine_eval(char *operations,int operations_size, char *input,int *input_order)
+unsigned char evoluchine_eval(unsigned char *operations,int operations_size, unsigned char *input,int *input_order)
 {
-  char SOM=0;
+  unsigned char SOM=0;
   int op;
-  char mulregister=1;
-  char cop;
+  unsigned char mulregister=input[0];
+  unsigned char cop;
   for(op=0; op!= operations_size; op++)
   {
      cop = operations[op];
      switch(cop){
         case SUM:
-          
-          SOM += mulregister;
-          if (SOM >1 ) 
-            SOM=0;
-          mulregister=1;
+          //SOM=MAX(mulregister,input[input_order[op]]);
+          SOM+=mulregister;
+          if ( (op+1) < operations_size)
+            mulregister=input[input_order[op+1]];
           break;
         case MULTIPLY:
-          mulregister*=input[input_order[op]];
+          mulregister =MIN(mulregister,input[input_order[op]]);
           break;
         case NEGATE_MULTIPLY:
-          mulregister*=!input[input_order[op]];
+          mulregister =MIN(mulregister,255-input[input_order[op]]); 
           break;
         case NEGATE_SUM:
-          SOM += !mulregister;
-          if (SOM >1 ) 
-            SOM=0;
-          break;
+           //SOM=MAX(255-mulregister,input[input_order[op]]);
+           SOM+=255-mulregister;
+           if ( (op+1) < operations_size)
+             mulregister=input[input_order[op+1]]; 
+           break;
      }
   }
   return SOM;
 }
-float evoluchine_batch_evaluate(char *operations,
+float evoluchine_batch_evaluate(unsigned char *operations,
                             int *inputs_order,
                             int operations_size ,
                             int inputs_size,
-                            char *inputs,
-                            char *ground_thruts,
+                            unsigned char *inputs,
+                            unsigned char *ground_thruts,
                             int batch_size){
   float err=0;
-  char res;
+  unsigned char res;
   for(int b=0; b != batch_size; b++)
   {
     res = evoluchine_eval(operations,
@@ -65,18 +66,21 @@ float evoluchine_batch_evaluate(char *operations,
                           (inputs+b*inputs_size),
                           inputs_order);
     if (res != ground_thruts[b])
+    {
+      printf("GOT %d EXPECTED %d\n",res,ground_thruts[b]);
       err++;
+    }
   }
   return err/batch_size;
 }
 
 
-void evoluchine_batch_solve(char *operations,
+void evoluchine_batch_solve(unsigned char *operations,
                             int *inputs_order,
                             int operations_size ,
                             int inputs_size,
-                            char *inputs,
-                            char *ground_thruts,
+                            unsigned char *inputs,
+                            unsigned char *ground_thruts,
                             int batch_size,
                             int epochs){
 
@@ -86,7 +90,7 @@ void evoluchine_batch_solve(char *operations,
   lastError=evoluchine_batch_evaluate(operations,inputs_order,operations_size,
                                       inputs_size,inputs,ground_thruts,batch_size);
 
-  char *mutated_operations = (char *)malloc(operations_size);
+  unsigned char *mutated_operations = (unsigned char *)malloc(operations_size);
   int  *mutated_order      = (int  *)malloc(sizeof(int)*operations_size);
   
   for (int e=0; e!= epochs; e++){
--- evoluchine.h
+++ evoluchine.h
@@ -4,21 +4,24 @@
 #define NEGATE_MULTIPLY 1 
 #define SUM  2
 #define NEGATE_SUM 3
-void evoluchine_randomize(char *operations,int operations_size, int *inputs_order, int input_size);
-char evoluchine_eval(char *operations,int operations_size, char *input,int *input_order);
-float evoluchine_batch_evaluate(char *operations,
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+void evoluchine_randomize(unsigned char *operations,int operations_size, int *inputs_order, int input_size);
+unsigned char evoluchine_eval(unsigned char *operations,int operations_size, unsigned char *input,int *input_order);
+float evoluchine_batch_evaluate(unsigned char *operations,
                             int *inputs_order,
                             int operations_size ,
                             int inputs_size,
-                            char *inputs,
-                            char *ground_thruts,
+                            unsigned char *inputs,
+                            unsigned char *ground_thruts,
                             int batch_size); 
-void evoluchine_batch_solve(char *operations,
+void evoluchine_batch_solve(unsigned char *operations,
                             int *inputs_order,
                             int operations_size ,
                             int inputs_size,
-                            char *inputs,
-                            char *ground_thruts,
+                            unsigned char *inputs,
+                            unsigned char *ground_thruts,
                             int batch_size,
                             int epochs);
 
